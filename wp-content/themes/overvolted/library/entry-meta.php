0e646411dbf444818acf08530f766ac5c78daf9a
Cameron Newland <cameron@reasonmedia.com>
2015-06-06 23:29:34 -0700

    added clock icon

--- wp-content/themes/overvolted/library/entry-meta.php
+++ wp-content/themes/overvolted/library/entry-meta.php
@@ -3,7 +3,7 @@ if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
 		echo '
 		<div class="post-meta left">
-			<time class="updated" datetime="'. get_the_time( 'c' ) .'">'.get_the_date().'</time>
+			<time class="updated" datetime="'. get_the_time( 'c' ) .'"><span><i class="icon-time"></i></span> '.get_the_date().'</time>
 		</div>';
 	}
 endif;
