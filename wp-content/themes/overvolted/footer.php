6591e1931725b4abb8e0f36d48b392234d954f34
Cameron Newland <cameron@reasonmedia.com>
2015-06-07 16:59:02 -0700

    added logo to footer

--- wp-content/themes/overvolted/footer.php
+++ wp-content/themes/overvolted/footer.php
@@ -1,7 +1,12 @@
 </section>
 <footer class="row">
 	<?php do_action( 'foundationpress_before_footer' ); ?>
-	<?php dynamic_sidebar( 'footer-widgets' ); ?>
+	<?php // dynamic_sidebar( 'footer-widgets' ); ?>
+	<div class="column small-12 text-center">
+		<a href="<?php echo home_url(); ?>">
+			<img class="logo" src="/wp-content/themes/overvolted/assets/img/logo.png">
+		</a>
+	</div>
 	<?php do_action( 'foundationpress_after_footer' ); ?>
 </footer>
 <a class="exit-off-canvas"></a>
