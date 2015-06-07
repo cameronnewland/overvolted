4bf7ceee7e0123f43d6819d52f81c1f94abfd7c8
Cameron Newland <cameron@reasonmedia.com>
2015-06-06 20:17:56 -0700

    fixed topbar hover and positioned the social sharing area

--- wp-content/themes/overvolted/parts/top-bar.php
+++ wp-content/themes/overvolted/parts/top-bar.php
@@ -1,9 +1,13 @@
 <div class="top-bar-container contain-to-grid show-for-medium-up">
     <nav class="top-bar" data-topbar role="navigation">
-        <a href="<?php echo home_url(); ?>"><img src="/wp-content/themes/overvolted/assets/img/logo.png"></a>
         <section class="top-bar-section">
             <?php foundationpress_top_bar_l(); ?>
             <?php foundationpress_top_bar_r(); ?>
+            <ul class="left">
+            	<li>
+            		<a href="<?php echo home_url(); ?>"><img src="/wp-content/themes/overvolted/assets/img/logo.png"></a>
+            	</li>
+            </ul>
 			<!-- Right Nav Section -->
 			<ul class="right">
 				<li>
