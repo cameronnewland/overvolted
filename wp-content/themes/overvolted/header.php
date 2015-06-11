a0c512665a92e9e2bc1eb7dbdb0cdf976ba89bd1
Cameron Newland <cameron@reasonmedia.com>
2015-06-10 21:40:56 -0700

    latest

--- wp-content/themes/overvolted/header.php
+++ wp-content/themes/overvolted/header.php
@@ -40,7 +40,7 @@
 		<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/mstile-144x144.png">
 		<meta name="theme-color" content="#ffffff">
 		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<?php wp_head(); ?>
 		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js"></script>
 		<script>
--- wp-content/themes/overvolted/parts/off-canvas-menu.php
+++ wp-content/themes/overvolted/parts/off-canvas-menu.php
@@ -1,12 +1,21 @@
 <aside class="left-off-canvas-menu" aria-hidden="true">
     <?php foundationpress_mobile_off_canvas(); ?>
     <li>
-    	<a href="/">Latest Ebike News</a>
+    	<a href="/"><i class="fa fa-motorcycle"></i> Latest Ebike News</a>
     </li>
     <li>
-    	<a href="/subscribe">Subscribe</a>
+    	<a href="/subscribe"><i class="fa fa-newspaper-o"></i> Subscribe</a>
     </li>
     <li>
-    	<a href="/">Contact</a>
+        <a href="http://instagram.com/overvolted"><i class="fa fa-instagram"></i> Follow On Instagram</a>
+    </li>
+    <li>
+        <a href="http://facebook.com/overvolted"><i class="fa fa-facebook-official"></i> Follow On Facebook</a>
+    </li>
+    <li>
+        <a href="http://twitter.com/overvolted"><i class="fa fa-twitter-square"></i> Follow On Twitter</a>
+    </li>
+    <li>
+    	<a href="/"><i class="fa fa-envelope-o"></i> Contact</a>
     </li>
 </aside>
