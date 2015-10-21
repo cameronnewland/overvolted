51d54926dc4aeea224c3e6baf40f2a1b84183285
Cameron Newland <cameron@reasonmedia.com>
2015-10-20 23:50:31 -0700

    fixed an issue for shops

--- wp-content/themes/overvolted/content-shops.php
+++ wp-content/themes/overvolted/content-shops.php
@@ -14,14 +14,16 @@
 	else: ?>
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/no-photo-yet.png">
 <?php endif ?>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	</header>
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
+	<div class="post-body">
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div>
+		<footer>
+			<?php foundationpress_entry_meta(); ?>
+			<a class="button read-more right" href="<?php the_permalink(); ?>">READ MORE</a>
+		</footer>
 	</div>
-	<footer>
-		<?php foundationpress_entry_meta(); ?>
-		<a class="button read-more right" href="<?php the_permalink(); ?>">READ MORE</a>
-	</footer>
 	<div class="clearfix"></div>
 </article>
